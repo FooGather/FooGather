a97019740985f39402eebbcdac2c07d120746c07
gkim <kimkm0419@naver.com>
2022-06-26 04:09:00 +0900

    Docker setting

---
+++ foogather/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+#
+# class Restaurant(models.Model):
+#     food =
+#     food_img =
+#     restaurant =
+#     category =
+#     menu_img =
+#     max_cnt =
+#     restaurant_s_desc =
+#     restaurant_desc =
+#
+#     def __str__(self):
+#         return '[{}] {}'.format(self., self.)
+#
+# class Category(models.Model):
+#     category =
+#
+#     def __str__(self):
+#         return '[{}] {}'.format(self., self.)
+#
+#
+# class (models.Model):
+#
+#
+#     def __str__(self):
+#         return '[{}] {}'.format(self.part, self.candidate_name)
