ded1b10a92097028afe54e47d82d4498b342e422
JSpiner <jspiner@naver.com>
2022-06-26 00:05:42 +0900

    Splash-Login 화면 뼈대를 구성합니다.

--- android/app/src/main/java/io/jspiner/foogather/ui/login/LoginActivity.kt
+++ android/app/src/main/java/io/jspiner/foogather/ui/login/LoginActivity.kt
@@ -1,6 +1,39 @@
 package io.jspiner.foogather.ui.login
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import io.jspiner.foogather.base.BaseActivity
+import io.jspiner.foogather.ui.main.MainActivity
+import io.jspiner.foogather.ui.theme.FooGatherTheme
 
 class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FooGatherTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginView(
+                        onLoginClick = ::onLoginClick
+                    )
+                }
+            }
+        }
+    }
+
+    private fun onLoginClick() {
+        // TODO 카카오 로그인
+        startActivity(
+            Intent(this, MainActivity::class.java)
+        )
+        finish()
+    }
 }
